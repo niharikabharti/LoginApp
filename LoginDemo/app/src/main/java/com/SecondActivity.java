a2aeb018c885868fdf5d859fcb4529c7369c22ef
niharikabharti <niharikabharti2001@gmail.com>
2021-05-22 16:06:10 +0530

    first commit

---
+++ LoginDemo/app/src/main/java/com/SecondActivity.java
@@ -0,0 +1,18 @@
+package com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.logindemo.R;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
